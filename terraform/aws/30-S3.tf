010a87df430d2da5a379d444d3e738a00ee8bc28
gerardVM <valverdegerard@gmail.com>
2025-01-18 23:23:41 +0100

    Making s3.tf versatile

--- terraform/aws/30-S3.tf
+++ terraform/aws/30-S3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket" {
-    for_each = { for bucket in local.aws.buckets : bucket => bucket }
+    for_each = { for bucket in try(local.aws.buckets, {}) : bucket => bucket }
 
     bucket = each.value
 }
